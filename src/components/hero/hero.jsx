7a5e9202aa43eb9bcf348e69d0b9b44de6e8f407
HiImKevin <brandnew121@gmail.com>
2021-08-27 15:42:12 -0500

    test image

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -9,8 +9,7 @@ const Hero = ({
   myText,
   myText2,
   myText3,
-  myText4,
-  myText5
+  myText4
 }) => {
   return (
     <div className="relative bg-white overflow-hidden">
@@ -111,7 +110,7 @@ const Hero = ({
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <img
           className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-          src={myText5}
+          src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
           alt=""
         />
       </div>
