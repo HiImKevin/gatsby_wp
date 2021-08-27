4a429729c705aa62ae839fb782fadf0cea2e7c13
HiImKevin <brandnew121@gmail.com>
2021-08-27 15:48:13 -0500

    update gatsby image

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -4,6 +4,7 @@ import * as React from "react"
 import PropTypes from 'prop-types';
 import { Popover, Transition } from '@headlessui/react'
 import { XIcon } from '@heroicons/react/outline'
+import { StaticImage } from "gatsby-plugin-image"
 
 const Hero = ({
   myText,
@@ -108,11 +109,7 @@ const Hero = ({
         </div>
       </div>
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-        <img
-          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-          src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
-          alt=""
-        />
+        <StaticImage alt="" className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80" />
       </div>
     </div>
   )
