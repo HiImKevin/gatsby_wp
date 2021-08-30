f2c3c3ae957888ef07bcd270ff62d608e03a7402
HiImKevin <brandnew121@gmail.com>
2021-08-30 17:37:55 -0500

    add svg logic to footermenu

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { Fragment } from "react"
 import { Popover, Transition } from "@headlessui/react"
+import { Link } from "gatsby"
 import {
   BookmarkAltIcon,
   CalendarIcon,
@@ -93,14 +94,14 @@ const Header = () => {
           <div className="max-w-7xl mx-auto px-4 sm:px-6">
             <div className="flex justify-between items-center border-b-2 border-gray-100 py-3 md:justify-start md:space-x-10">
               <div className="flex justify-start lg:w-0 lg:flex-1">
-                <a href="#">
-                  <span className="sr-only">Workflow</span>
+                <span className="sr-only">Workflow</span>
+                <Link to="/">
                   <img
                     className="h-8 w-auto sm:h-10"
                     src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
                     alt=""
                   />
-                </a>
+                </Link>
               </div>
               <div className="-mr-2 -my-2 md:hidden">
                 <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
