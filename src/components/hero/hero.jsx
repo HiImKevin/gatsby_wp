6afc8966ffcf05db347a00da24029d2630bdfa16
HiImKevin <brandnew121@gmail.com>
2021-08-31 14:32:08 -0500

    add loading eager to hero

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -109,7 +109,7 @@ const Hero = ({
         </div>
       </div>
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-        <StaticImage alt="" className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80" />
+        <StaticImage alt="" className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" loading={'eager'} src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80" />
       </div>
     </div>
   )
